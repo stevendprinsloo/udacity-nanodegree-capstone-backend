2c84eb4d62eb8d6fb293fdb9e2d3bae5ba7f1339
Steven Prinsloo <steven.prinsloo@creationlabs.co.za>
2023-06-28 13:42:05 +0200

    Updates to .gitignore

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/casting_agency"
-export AUTH0_DOMAIN='dev-ciko6qxd.us.auth0.com'
-export ALGORITHMS=`['RS256']`
-export API_AUDIENCE='casting_agency'
-
-echo "setup.sh script executed successfully!"
--- test-movie-import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO movies (id, title, release_date) VALUES(1, 'Test Movie', '2022-04-06');
-INSERT INTO actors (id, title, gender) VALUES(1, 'Tom Cruise', 'male');
